7a87b0ee44740b665c93d67d499e406b21bc11db
toncy11 <ajagodic1990@gmail.com>
2020-12-30 00:02:03 +0100

    no message

--- gps/src/main/java/com/openclassrooms/ocproject8/gps/service/GpsService.java
+++ gps/src/main/java/com/openclassrooms/ocproject8/gps/service/GpsService.java
@@ -47,6 +47,7 @@ public class GpsService {
 		VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 		user.addToVisitedLocations(visitedLocation);
 		//rewardsService.calculateRewards(user);
+		//invalid
 		return visitedLocation;
 	}
 	
