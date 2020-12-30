7041a0c45832fa05f6d8be0ced5e01937bc634c0
Antonio Jagodić <ajagodic1990@gmail.com>
2020-12-30 13:50:15 +0100

    Add futures to the gps app

--- gps/src/main/java/com/openclassrooms/ocproject8/gps/service/GpsService.java
+++ gps/src/main/java/com/openclassrooms/ocproject8/gps/service/GpsService.java
@@ -7,11 +7,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
-import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.springframework.stereotype.Service;
 
-import com.openclassrooms.ocproject8.shared.domain.LocationDTO;
 import com.openclassrooms.ocproject8.shared.domain.User;
 import com.openclassrooms.ocproject8.shared.domain.UserEntity;
 import com.openclassrooms.ocproject8.shared.domain.VisitedLocationDTO;
@@ -23,18 +26,17 @@ import gpsUtil.location.VisitedLocation;
 @Service
 public class GpsService {
 
-
 	private UserService userService;
-
+	
 	private final GpsUtil gpsUtil = new GpsUtil();
-
+	
 	public GpsService(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	public VisitedLocation getUserLocation(String userName) {
 		Optional<UserEntity> userEntity = userService.getUser(userName);
-		if(userEntity.isPresent()) {
+		if (userEntity.isPresent()) {
 			User user = new User(userEntity.get());
 			VisitedLocation visitedLocation = (user.getVisitedLocations().size() > 0) ? user.getLastVisitedLocation()
 					: trackUserLocation(user);
@@ -46,24 +48,54 @@ public class GpsService {
 	public VisitedLocation trackUserLocation(User user) {
 		VisitedLocation visitedLocation = gpsUtil.getUserLocation(user.getUserId());
 		user.addToVisitedLocations(visitedLocation);
-		//rewardsService.calculateRewards(user);
+		// rewardsService.calculateRewards(user);
 		return visitedLocation;
 	}
-	
+
 	public List<VisitedLocationDTO> getAllUsersLocations() {
 		return calculateAllUserLocations();
 	}
-	
+
 	public List<VisitedLocationDTO> calculateAllUserLocations() {
+
 		List<VisitedLocationDTO> visitedLocations = new ArrayList<>();
-		for (UserEntity userEntity : userService.getAllUsers()) {	
-			LocationDTO locationDTO = new LocationDTO(generateRandomLatitude(), generateRandomLongitude());
-			visitedLocations.add(new VisitedLocationDTO(UUID.fromString(userEntity.getUserId()),getRandomTime(), locationDTO));
+
+		ExecutorService executor = Executors.newFixedThreadPool(1000);
+		// create a list to hold the Future object associated with Callable
+		List<Future<VisitedLocation>> list = new ArrayList<Future<VisitedLocation>>();
+		// Create Callable instance
+		Callable<VisitedLocation> callable = new GpsTask(gpsUtil);
+		for (int i = 0; i < 10000; i++) {
+			// submit Callable tasks to be executed by thread pool
+			Future<VisitedLocation> future = executor.submit(callable);
+			// add Future to the list, we can get return value using Future
+			list.add(future);
 		}
-		return visitedLocations;
+		for (Future<VisitedLocation> future : list) {
+			try {
+				// print the return value of future, output delay in console because
+				// Future.get() waits for a task to be a complited
+
+				// add location to list
+				visitedLocations.add(future.get());
+				
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+		executor.shutdown();
+
+		/*
+		 * List<VisitedLocationDTO> visitedLocations = new ArrayList<>(); for
+		 * (UserEntity userEntity : userService.getAllUsers()) { LocationDTO locationDTO
+		 * = new LocationDTO(generateRandomLatitude(), generateRandomLongitude());
+		 * visitedLocations.add(new
+		 * VisitedLocationDTO(UUID.fromString(userEntity.getUserId()),getRandomTime(),
+		 * locationDTO)); } return visitedLocations;
+		 */
 
 	}
-	
+
 	private double generateRandomLongitude() {
 		double leftLimit = -180;
 		double rightLimit = 180;
@@ -80,6 +112,5 @@ public class GpsService {
 		LocalDateTime localDateTime = LocalDateTime.now().minusDays(new Random().nextInt(30));
 		return Date.from(localDateTime.toInstant(ZoneOffset.UTC));
 	}
-
 
 }
